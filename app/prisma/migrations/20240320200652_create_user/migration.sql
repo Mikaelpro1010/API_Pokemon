c5cc9e231e409eb8ce3f6d6fc7052c52677d4745
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-03-20 17:08:52 -0300

    Adicionando novas colunas na tabela de Users

--- app/prisma/migrations/20240320200652_create_user/migration.sql
+++ app/prisma/migrations/20240320200652_create_user/migration.sql
@@ -3,6 +3,9 @@ CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "name" VARCHAR(255),
     "email" VARCHAR(255) NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
