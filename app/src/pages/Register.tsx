fe7d9f7113d8e026bc074b60fe8a4ace763d62b9
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-03-22 08:12:52 -0300

    Criando a página de cadastro de usuário

---
+++ app/src/pages/Register.tsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import { supabase } from '../client';
+
+function Register() {
+  
+  const [formData,setFormData] = useState({
+    userName:'',email:'',password:''
+  })
+
+  console.log(formData)
+
+  function handleChange(event){
+    setFormData((prevFormData)=>{
+      return{
+        ...prevFormData,
+        [event.target.name]:event.target.value
+      }
+
+    })
+
+  }
+
+  async function handleSubmit(e){
+    e.preventDefault()
+
+    try {
+      const { data, error } = await supabase.auth.signUp(
+        {
+          email: formData.email,
+          password: formData.password,
+          options: {
+            data: {
+              full_name: formData.userName,
+            }
+          }
+        }
+      )
+      if (error) throw error
+      alert('Check your email for verification link')
+
+      
+    } catch (error) {
+      alert(error)
+    }
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input 
+          placeholder='Nome de usuário'
+          name='userName'
+          onChange={handleChange}
+        />
+
+        <input 
+          placeholder='Email'
+          name='email'
+          onChange={handleChange}
+        />
+
+        <input 
+          placeholder='Password'
+          name='password'
+          type="password"
+          onChange={handleChange}
+        />
+
+        <button type='submit'>
+          Submit
+        </button>
+
+
+      </form>
+      
+    </div>
+  )
+}
+
+export default Register;
+
