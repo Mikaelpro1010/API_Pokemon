37d3e5e8658a34035c131486da844ceb7a9e343e
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-03-23 16:57:46 -0300

    Estilizando os inputs de filtragem de pokemons

--- app/src/Components/Filter.tsx
+++ app/src/Components/Filter.tsx
@@ -3,20 +3,28 @@ import React from 'react';
 
 const Filter = ({ searchName, setSearchName, searchType, setSearchType }) => {
     return(
-        <div className="flex justify-end p-3">
-          <input
-            className="mx-4"
-            type="text"
-            placeholder="Nome"
-            value={searchName}
-            onChange={(e) => setSearchName(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Tipo"
-            value={searchType}
-            onChange={(e) => setSearchType(e.target.value)}
-          />
+        <div className="flex justify-end p-4 items-end">
+            <div className="space-y-3">
+                <div className="block mx-4">
+                    <h2 className="text-lg font-bold mr-4">Filtrar Pokémons:</h2>
+                </div>
+                <div className="block grid grid-cols-3 gap-4">
+                    <input
+                        className="p-2 mx-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent col-span-2"
+                        type="text"
+                        placeholder="Nome"
+                        value={searchName}
+                        onChange={(e) => setSearchName(e.target.value)}
+                    />
+                    <input
+                        className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                        type="text"
+                        placeholder="Tipo"
+                        value={searchType}
+                        onChange={(e) => setSearchType(e.target.value)}
+                    />
+                </div>
+            </div>
         </div>
     )
 }
