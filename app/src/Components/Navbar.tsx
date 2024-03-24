33fccdfcfc27d94bcdb2b4f9d50305051493806d
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-03-24 13:10:05 -0300

    Ajustado o layout do button Sign out da NavBar

--- app/src/Components/Navbar.tsx
+++ app/src/Components/Navbar.tsx
@@ -85,9 +85,20 @@ const Navbar = ({ onLogout }) => {
                   >
                     <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <Menu.Item>
-                        {({ active }) => (
-                          <button  className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')} onClick={onLogout}>Sign out</button>
-                        )}
+                      {({ active }) => (
+                        <button 
+                          className={classNames(
+                            active ? 'bg-gray-100' : '',
+                            'flex items-center justify-between w-full px-4 py-2 text-sm text-gray-700 hover:text-gray-900'
+                          )}
+                          onClick={onLogout}
+                        >
+                          <div className="flex items-center">
+                            <span>Sign out</span>
+                          </div>
+                          {/* Adicione qualquer outro elemento que você queira no lado direito */}
+                        </button>
+                      )}
                       </Menu.Item>
                     </Menu.Items>
                   </Transition>
