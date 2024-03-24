c790c3d1c6f3849779f072153e67eee9b199a610
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-03-24 17:37:02 -0300

    Adicionando efeitos no card e incrementando o cursor-pointer para indicar que o card pode ser clicado pelo usuário

--- app/src/Components/Card.tsx
+++ app/src/Components/Card.tsx
@@ -4,7 +4,7 @@ const Card: React.FC<{ pokemon: any; onClick: () => void }> = ({ pokemon, onClic
   const { id, name, image, type } = pokemon;
 
   return (
-    <div className="card bg-gray rounded-lg shadow-md p-6" onClick={onClick}>
+    <div className="card bg-gray rounded-lg shadow-md p-6 hover:shadow-xl transition duration-300 cursor-pointer" onClick={onClick}>
       <span className="card--id text-lg font-bold">#{id}</span>
       <div className="mt-4 flex justify-center">
         <img className="card--image mt-4" src={image} alt={name} />
